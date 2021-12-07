188f53d19e87e70253893b5bd5a9a1777b0c805e
unknown <pwamly2016@gmail.com>
2021-12-07 20:31:15 +0300

    token veirfication

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,9 @@
+# from typing_extensions import Required
 from flask import Flask,render_template,request,jsonify,make_response
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime,timedelta
 import jwt
+from functools import wraps
  
 app = Flask(__name__)
 
@@ -29,18 +31,42 @@ class User(db.Model):
         return f"{self.name}:{self.age}"
 
 
+def token_required(f):
+     @wraps(f)
+     def decorated(*args,**kwargs):
+         token = request.args.get('token')
+         if not token :
+             return jsonify({'message': 'Token is missing'}) , 403
+         
+         try :
+             data=jwt.decode(token, app.config['SECRET_KEY'], algorithm="HS256") 
+             print(data)
+
+         except Exception as e: # work on python 2.x
+               print(e)
+               return jsonify({'message': 'Token is Invalid'}),403
+              
+         return f(*args,**kwargs)
+     
+     return decorated    
  
 @app.route('/login', methods = ['POST'])
 def login():  
     # getting posted data and check for auth
     auth = request.authorization
     if auth and auth.password == '123':
-       token = jwt.encode({'user' : auth.username,'exp': datetime.utcnow() + timedelta(minutes=30) },app.config['SECRET_KEY'])
-       print(token)
+       token = jwt.encode({'user' : auth.username,'exp': datetime.utcnow() + timedelta(seconds=30) },app.config['SECRET_KEY'],algorithm="HS256")
+    #    print(token)
 
-       return jsonify({'token': token})
+       return jsonify({'token': token.decode('UTF-8')})
  
     return make_response('could not  verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
+@app.route('/protected', methods=['GET'])
+@token_required
+def protected():
+    return jsonify({'message': 'This is protected route'})
+
       
  
 if __name__ == '__main__':

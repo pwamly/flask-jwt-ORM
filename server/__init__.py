6f49aedceb677340d17fd59bf21ed2244e6d694d
unknown <pwamly2016@gmail.com>
2021-12-31 10:32:41 +0300

    adding cor for x platforms

--- server/__init__.py
+++ server/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_migrate import Migrate
 from .extensions import db
 from .commands import create_tables 
+from flask_cors import CORS
 from .routes import main
 
 def create_app(config_file='settings.py'):
     app= Flask(__name__)    
     app.config.from_pyfile(config_file)
     db.init_app(app)
+    CORS(app)
     migrate = Migrate(app, db)
     app.register_blueprint(main)
     app.cli.add_command(create_tables)

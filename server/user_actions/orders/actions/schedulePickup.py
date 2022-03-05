22adf0eb7a21b4c39360916355a1dd44bb14ba4b
pwamly <pwamly2016@gmail.com>
2022-03-05 16:03:25 +0300

    resolving issues

--- server/user_actions/orders/actions/schedulePickup.py
+++ server/user_actions/orders/actions/schedulePickup.py
@@ -20,25 +20,25 @@ def schedulePickup(orderid, data, db):
         orderid=orderid)
     if itemslist.count() > scheduledItems.count():
         if isinstance(listitems, list):
-
+            print('iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',listitems)
             for item in listitems:
                 item = Item.query.filter_by(itemid=item['itemid']).first()
                 order = Order.query.filter_by(orderid=orderid).first()
                 try:
-
                     item.driverId = driverId
-                    item.pickupScheduled = True
+                    # item.pickupScheduled = True
                     item.status = 'Pickup Scheduled'
                     order.orderStatus = 'Partial Pickup Scheduled'
                     item.scheduledPickuptime = datetime.now()
                     order.vehicleId = vehicleId
-                    db.session.add(order)
+                    db.session.add(order) 
                     db.session.add(item)
                     db.session.commit()
-                    return jsonify({'message': ' Pickup Scheduled'}), 200
+                    # return jsonify({'message': ' Pickup Scheduled'}), 200
                 except Exception as e:
                     print(e)
                     return jsonify({'message': 'Failed to schedule pickup'}), 403
+    
 
             return jsonify({'message': 'No item i'}), 403
 
@@ -46,7 +46,7 @@ def schedulePickup(orderid, data, db):
     print('all list.................',itemslist.count() )
     print('scheduled list gggggggggggg',scheduledItems.count())
     if order:
-        
+        print('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',itemslist.count() , scheduledItems.count())
         if itemslist.count() == scheduledItems.count():
             order.pickupScheduled = True
             order.orderStatus = 'Pickup Scheduled'
@@ -67,7 +67,7 @@ def schedulePickup(orderid, data, db):
 
             db.session.add(order)
             db.session.commit()
-            return jsonify({'message': 'pickup scheduled'}), 200
+            # return jsonify({'message': 'pickup scheduled'}), 200
 
         except Exception as e:
             print(e)

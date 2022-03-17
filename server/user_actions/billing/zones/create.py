160aaf39d0585b7dbdaf6af97f8c34843e602c3b
pwamly <pwamly2016@gmail.com>
2022-03-17 12:55:13 +0300

    resolving create order issues

--- server/user_actions/billing/zones/create.py
+++ server/user_actions/billing/zones/create.py
@@ -8,28 +8,31 @@ import uuid
 from werkzeug.security import generate_password_hash
 
 
-def createZone(data, db):
+def  createZone(data, db):
 
     zoneid = uuid.uuid4()
     name = data['name']
     description = data['description']
+    # zone =  Zone.query.filter_by(zoneid=zoneid).first()
 
-    zone = Zone.query.filter_by(zoneid=zoneid).first()
+    # if not zone:
+    try:
+        zones = Zone(zoneid=zoneid, name=name, description=description)
 
-    if not zone:
-        try:
-            zones = Zone(zoneid=zoneid, name=name, description=description)
+        zones.created = datetime.utcnow()
+        
+        db.session.add(zones)
+        db.session.commit()
+        print('Zone created')
+        return  jsonify({'message': 'zone created '}), 200
+        
 
-            zones.created = datetime.utcnow
-            
-            db.session.add(zones)
-            db.session.commit()
+    except Exception as e:
+        print(e.code)
+        if e.code=='gkpj':
+            print('Zone already exist! ')
+            return jsonify({'message': 'Zone already exist!'}), 407
+        return jsonify({'message': 'Failed to create zone'}), 403
+      
 
-            return 'Zone created'
-
-        except Exception as e:
-            print(e)
-            return jsonify({'message': 'Failed to create zone'}), 403
-            pass
-
-    return jsonify({'message': 'Zone already exist!'}), 407
+    # return jsonify({'message': 'Zone already exist!'}), 407
